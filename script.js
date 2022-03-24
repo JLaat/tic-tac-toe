abd64ddb76e808f62b0b11ad02147485d208ed19
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-03-24 16:46:20 +0200

    Game is now playable, started to think about creating AI

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ const gameBoard = (() => {
          [0, 4, 8], [2, 4, 6]];
 
 
-
     const getValue = (index) => {
         return boardValues[index];
     }
@@ -27,16 +26,23 @@ const gameBoard = (() => {
         boardValues[index] = sign;
         console.log(boardValues[index]);
     }
+    const resetArray = () => {
+        boardValues = ["", "", "", "", "", "", "", "", ""];
+    }
 
-    return {getValue, setValue, winningIndexes};
+    return {getValue, setValue, winningIndexes, resetArray};
 })();
 
 
 // All DOM stuff here.
 const domController = (() => {
     let boardSigns = document.querySelectorAll(".boardSpot");
+    const btnRestart = document.querySelector(".restart");
+    let playerOneName = document.querySelector("#playerOne").value;
+    let playerTwoName = document.querySelector("#playerTwo").value;
 
 
+    // Event listeners for board spots
     boardSigns.forEach(sign => sign.addEventListener("click", function () {
         if (gameBoard.getValue(sign.dataset.index) !== ""){
             console.log("nonii")
@@ -47,21 +53,53 @@ const domController = (() => {
         }
     }));
 
-    return {};
+
+    // Event listener for restart button
+    btnRestart.addEventListener("click", () => {
+        document.querySelector(".modal-container").style.display ="none";
+        resetBoard();
+    })
+
+    const resetBoard = () => {
+        gameBoard.resetArray();
+        boardSigns.forEach(sign => sign.textContent = "");
+        btnRestart.disabled = true;
+    }
+
+    const announceWinner = () => {
+        document.querySelector(".modal-container").style.display ="inline";
+        if (gameController.playerOneTurn) {
+            document.querySelector(".modal-text").textContent = `Nice ${playerOneName}, you won!`;
+        } else {
+            document.querySelector(".modal-text").textContent = `Nice ${playerTwoName}, you won!`;
+        }
+        btnRestart.disabled = false;
+    }
+    const announceTie = () => {
+        document.querySelector(".modal-container").style.display ="inline";
+        document.querySelector(".modal-text").textContent = "It's a tie!";
+        btnRestart.disabled = false;
+    }
+
+    return {announceWinner, announceTie};
 })();
+
 
 // Module for controlling gameflow.
 const gameController = (() => {
     const playerOne = player("X");
     const playerTwo = player("O");
     let playerOneTurn = true;
+    let roundCount = 0;
 
     const makeMove = (index) => {
         if (playerOneTurn && gameBoard.getValue(index) === "") {
             playerOneTurn = false;
+            roundCount++;
             return playerOne.getSign(index);
         } else if (!playerOneTurn && gameBoard.getValue(index) === ""){
             playerOneTurn = true;
+            roundCount++;
             return playerTwo.getSign(index);
         }
         else {
@@ -79,14 +117,21 @@ const gameController = (() => {
             }
             if (areEquals(partOfBoard)){
                 console.log("game over");
+                domController.announceWinner();
+                playerOneTurn = true;
+                roundCount = 0;
             }
             partOfBoard = [];
+        }
+        if (roundCount === 9){
+            domController.announceTie();
+            roundCount = 0;
         }
     };
 
     const areEquals = (array) => {
         if (array[0] === array[1] && array[1] === array[2]
-            && array[0] === ("X" || "O ")){
+            && (array[0] === "X" || array[0] === "O")){
             return true;
         } else {
             return false;
@@ -94,7 +139,7 @@ const gameController = (() => {
     };
 
 
-    return {makeMove, checkWinner};
+    return {makeMove, checkWinner, playerOneTurn};
 })();
 
 
